a3973beaaa83b58bebad8caa3fbbbc879341560c
VanillaBase1lb <yuvrajmishra7777777@gmail.com>
2021-11-26 12:03:05 +0530

    lab4 partially

---
+++ lab7/19XJ1A0576.cpp
@@ -0,0 +1,180 @@
+#include <iostream>
+#include <cstdlib>
+#include <vector>
+#include <ctime>
+
+#define LOOPS 2000
+#define MEM_SIZE 1048576 // 1MB
+
+using namespace std;
+
+class Process {
+    public:
+    int start = -1; // starting location in the memory
+    int size = -1; // size of the process
+};
+
+// keeps track of all free memory spaces in the memory
+class MemoryHole {
+    public:
+    int start;
+    int size;
+};
+
+vector<MemoryHole> memory_hole(0); // all free memory spaces aka holes
+vector<Process> process_list(0); // list of all processes
+
+// creates new processes and returns its size
+int processSizeGenerator(int mode) {
+    switch (mode) {
+    case 1:
+        return rand() % 1024 + 1; // generates random number between 1 and 1024 both inclusive
+        break;
+    case 2:
+        return rand() % 100 + 1; // generates random number between 1 and 100 both inclusive
+        break;
+    case 3:
+        return rand() % 501 + 500; // generates random number between 500 and 1000 both inclusive
+        break;
+    default:
+        return 0;
+        break;
+    }
+}
+
+// inserts processes in process_list in ascending order
+void processInsert(int process_size, int process_start) {
+    printf("%d %d\n", process_start, process_size);
+    Process *temp_process = new Process;
+    temp_process->size = process_size;
+    temp_process->start = process_start;
+
+    if (process_list.size() == 0) {
+        process_list.push_back(*temp_process);
+        return;
+    }
+
+    for (int i = 0; i < process_list.size(); i++) { // sort by start
+        if (process_start < process_list[i].start) {
+            process_list.insert(process_list.begin() + i, *temp_process);
+            return;
+        }
+    }
+
+    process_list.push_back(*temp_process); // insert largest value at the end
+    delete temp_process;
+}
+
+// adds process to the memory by removing/resizing holes and returns starting position of the process
+int processAdd(int mode, int process_size) {
+    int temp;
+
+    switch (mode) {
+    case 1: // best fit
+        break;
+    case 2: // worst fit
+        break;
+    case 3: // first fit
+        for (int i = 0; i < memory_hole.size(); i++) { // go through all holes
+            if (memory_hole[i].size > process_size) { // if the hole is big enough, shrink it
+                temp = memory_hole[i].start;
+                memory_hole[i].start += process_size;
+                memory_hole[i].size -= process_size;
+                return temp;
+            }
+            else if (memory_hole[i].size == process_size) { // if the hole is just big enough, remove it
+                temp = memory_hole[i].start;
+                memory_hole.erase(memory_hole.begin() + i);
+                return temp;
+            }
+        }
+        break;
+    default:
+        break;
+    }
+    return -1;
+}
+
+// determines whether the new process can fit in memory
+bool processCanFit(int process_size) {
+    for (int i = 0; i < memory_hole.size(); i++) {
+        if (process_size <= memory_hole[i].size) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+// inserts holes in memory_hole in ascending order
+void holeInsert(int hole_size, int hole_start) {
+    MemoryHole *temp_hole = new MemoryHole;
+    temp_hole->start = hole_start;
+    temp_hole->size = hole_size;
+
+    if (memory_hole.size() == 0) {
+        memory_hole.push_back(*temp_hole);
+        return;
+    }
+
+    for (int i = 0; i < memory_hole.size(); i++) { // sort by start
+        if (hole_start < memory_hole[i].start) {
+            memory_hole.insert(memory_hole.begin() + i, *temp_hole);
+            return;
+        }
+    }
+
+    memory_hole.push_back(*temp_hole);
+    delete temp_hole;
+}
+
+// removes a process from memory randomly
+void processRemove() {
+    int r = rand() % process_list.size(); // select a process at random
+    bool touching_hole = false;
+
+    for (int i = 0; i < memory_hole.size(); i++) {
+        if (process_list[r].start == memory_hole[i].start + memory_hole[i].size) { // if there is a hole above the process
+            touching_hole = true;
+            if (process_list[r].start + process_list[r].size == memory_hole[i + 1].start) { // if there is a hole below also
+                memory_hole[i].size += process_list[r].size + memory_hole[i + 1].size; // expand the upper hole till bottom hole ends
+                memory_hole.erase(memory_hole.begin() + i + 1); // remove the bottom hole
+            }
+            else { // if there is no hole below the process
+                memory_hole[i].size += process_list[r].size; // expand the hole till process ends
+            }
+        }
+
+        else if (process_list[r].start + process_list[r].size == memory_hole[i].start) { // if there is a hole below the process
+            touching_hole = true;
+            memory_hole[i].start = process_list[r].start; // hole starts from where process started
+            memory_hole[i].size += process_list[r].size; // hole size is increased
+        }
+    }
+    if (!touching_hole) {
+        printf("inserting hole\n");
+        holeInsert(process_list[r].size, process_list[r].start);
+    }
+    process_list.erase(process_list.begin() + r); // remove the process
+}
+
+int main(int argc, char **argv) {
+    srand(time(0)); // generate random seed. this changes every second
+
+    holeInsert(MEM_SIZE, 0); // initialize empty memory as the first hole
+
+    for (int i = 0; i < LOOPS; i++) { // run the simulation LOOPS number of times
+        int process_size = processSizeGenerator(atoi(argv[1])); // first generate a random process size
+        while (!processCanFit(process_size)) { // check if the process can fit in existing holes, continue until it can
+            processRemove(); // remove a process randolmly
+        }
+        int process_start = processAdd(atoi(argv[2]), process_size); // find out which hole to add the process to
+        processInsert(process_size, process_start); // add the process to process_list
+    }
+
+    /* for (int i = 0; i < process_list.size(); i++) { */
+    /*     printf("%d %d\n", process_list[i].start, process_list[i].size); */
+    /* } */
+
+    return 0;
+}
